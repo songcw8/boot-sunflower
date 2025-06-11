1fbb406b0367f3e8e9835101bc1d2cf27e73f6e8
songcw8 <ben990158@gmail.com>
2025-06-11 10:11:36 +0900

    Initial commit

---
+++ src/main/java/org/example/bootsunflower/BootSunflowerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootsunflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSunflowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSunflowerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootsunflower/BootSunflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootsunflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSunflowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
